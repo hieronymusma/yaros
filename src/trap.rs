86129581de38139695f60d57af59681924678821
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2023-07-05 12:54:55 +0200

    Refactor mcause into own struct

--- src/trap.rs
+++ src/trap.rs
@@ -3,19 +3,76 @@ use crate::{
     print, println, uart,
 };
 
-#[no_mangle]
-extern "C" fn machine_mode_trap(mcause: usize, mtval: usize) {
-    let is_interrupt = (mcause >> 63) == 1;
+#[repr(transparent)]
+struct MCause(usize);
 
-    if is_interrupt {
+impl MCause {
+    fn is_asynchronous(&self) -> bool {
+        self.0 >> 63 == 1
+    }
+
+    fn get_exception_code(&self) -> usize {
+        self.0 << 1 >> 1
+    }
+
+    fn get_reason(&self) -> &'static str {
+        let is_asynchronous = self.is_asynchronous();
+
+        if is_asynchronous {
+            match self.get_exception_code() {
+                0 => "Reserved",
+                1 => "Supervisor software interrupt",
+                2 => "Reserved",
+                3 => "Machine software interrupt",
+                4 => "Reserved",
+                5 => "Supervisor timer interrupt",
+                6 => "Reserved",
+                7 => "Machine timer interrupt",
+                8 => "Reserved",
+                9 => "Supervisor external interrupt",
+                10 => "Reserved",
+                11 => "Machine external interrupt",
+                12..=15 => "Reserved",
+                _ => "Designated for platform use",
+            }
+        } else {
+            match self.get_exception_code() {
+                0 => "Instruction address misaligned",
+                1 => "Instruction access fault",
+                2 => "Illegal instruction",
+                3 => "Breakpoint",
+                4 => "Load address misaligned",
+                5 => "Load access fault",
+                6 => "Store/AMO address misaligned",
+                7 => "Store/AMO access fault",
+                8 => "Environment call from U-mode",
+                9 => "Environment call from S-mode",
+                10 => "Reserved",
+                11 => "Environment call from M-Mode",
+                12 => "Instruction page fault",
+                13 => "Load page fault",
+                14 => "Reserved",
+                15 => "Store/AMO page fault",
+                16..=23 => "Reserved",
+                24..=31 => "Designated for custom use",
+                32..=47 => "Reserved",
+                48..=63 => "Designated for custom use",
+                _ => "Reserved",
+            }
+        }
+    }
+}
+
+#[no_mangle]
+extern "C" fn machine_mode_trap(mcause: MCause, mtval: usize) {
+    if mcause.is_asynchronous() {
         println!(
             "Asynchronous Machine mode trap occurred! (mcause: {} (Reason: {})) (mtval: 0x{:x})",
-            (mcause << 1) >> 1,
-            get_reason(mcause),
+            mcause.get_exception_code(),
+            mcause.get_reason(),
             mtval
         );
-        let mcause = (mcause << 1) >> 1;
-        match mcause {
+        match mcause.get_exception_code() {
             11 => {
                 let plic_interrupt =
                     plic::get_next_pending().expect("There should be a pending interrupt.");
@@ -46,68 +103,19 @@ extern "C" fn machine_mode_trap(mcause: usize, mtval: usize) {
     } else {
         panic!(
             "Machine mode trap occurred! (mcause: {} (Reason: {})) (mtval: 0x{:x})",
-            mcause,
-            get_reason(mcause),
+            mcause.get_exception_code(),
+            mcause.get_reason(),
             mtval
         );
     }
 }
 
 #[no_mangle]
-extern "C" fn supervisor_mode_trap(mcause: usize, mtval: usize) {
+extern "C" fn supervisor_mode_trap(mcause: MCause, mtval: usize) {
     panic!(
         "Supervisor mode trap occurred! (mcause: {} (Reason: {})) (mtval: 0x{:x})",
-        mcause,
-        get_reason(mcause),
+        mcause.get_exception_code(),
+        mcause.get_reason(),
         mtval
     );
-}
-
-fn get_reason(mcause: usize) -> &'static str {
-    let is_interrupt = (mcause >> 63) == 1;
-
-    let mcause_with_cleared_interrupt_bit = (mcause << 1) >> 1;
-
-    if is_interrupt {
-        match mcause_with_cleared_interrupt_bit {
-            0 => "Reserved",
-            1 => "Supervisor software interrupt",
-            2 => "Reserved",
-            3 => "Machine software interrupt",
-            4 => "Reserved",
-            5 => "Supervisor timer interrupt",
-            6 => "Reserved",
-            7 => "Machine timer interrupt",
-            8 => "Reserved",
-            9 => "Supervisor external interrupt",
-            10 => "Reserved",
-            11 => "Machine external interrupt",
-            12..=15 => "Reserved",
-            _ => "Designated for platform use",
-        }
-    } else {
-        match mcause_with_cleared_interrupt_bit {
-            0 => "Instruction address misaligned",
-            1 => "Instruction access fault",
-            2 => "Illegal instruction",
-            3 => "Breakpoint",
-            4 => "Load address misaligned",
-            5 => "Load access fault",
-            6 => "Store/AMO address misaligned",
-            7 => "Store/AMO access fault",
-            8 => "Environment call from U-mode",
-            9 => "Environment call from S-mode",
-            10 => "Reserved",
-            11 => "Environment call from M-Mode",
-            12 => "Instruction page fault",
-            13 => "Load page fault",
-            14 => "Reserved",
-            15 => "Store/AMO page fault",
-            16..=23 => "Reserved",
-            24..=31 => "Designated for custom use",
-            32..=47 => "Reserved",
-            48..=63 => "Designated for custom use",
-            _ => "Reserved",
-        }
-    }
 }
