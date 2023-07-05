e2aa25c1912d6a906060ca2521b7b88b8f8d7bac
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2023-07-05 19:55:56 +0200

    Implement qemu exit reset function

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -2,8 +2,6 @@ use crate::println;
 
 mod qemu_exit;
 
-pub use qemu_exit::TEST_DEVICE_ADDRESSS;
-
 pub fn test_runner(tests: &[&dyn Fn()]) -> ! {
     println!("Running {} tests", tests.len());
     for test in tests {
--- src/test/qemu_exit.rs
+++ src/test/qemu_exit.rs
@@ -1,6 +1,6 @@
 use crate::klibc::MMIO;
 
-pub const TEST_DEVICE_ADDRESSS: usize = 0x100000;
+const TEST_DEVICE_ADDRESSS: usize = 0x100000;
 
 const EXIT_SUCCESS_CODE: u32 = 0x5555;
 const EXIT_FAILURE_CODE: u32 = 0x3333;
@@ -17,5 +17,11 @@ pub fn exit_success() {
 pub fn exit_failure(code: u16) {
     unsafe {
         TEST_DEVICE.write(EXIT_FAILURE_CODE | ((code as u32) << 16));
+    }
+}
+
+pub fn exit_reset() {
+    unsafe {
+        TEST_DEVICE.write(EXIT_RESET_CODE);
     }
 }
