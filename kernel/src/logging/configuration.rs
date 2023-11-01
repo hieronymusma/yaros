064bc8c0afcd13823795369f783c38ffad7ab18b
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2023-11-01 18:55:22 +0100

    Add explanation for logging configuration

--- kernel/src/logging/configuration.rs
+++ kernel/src/logging/configuration.rs
@@ -1,6 +1,7 @@
-// This variable contains the list of modules that should be logged.
-const LOG_FOLLOWING_MODULES: &[&str] = &["kernel"];
+// This variable contains the list of modules that should be logged. If "kernel" is specified, every module is logged.
+const LOG_FOLLOWING_MODULES: &[&str] = &[];
 
+// TODO: This should be made compile-time, such that this thing doesn't need to be queried at runtime.
 pub fn should_log_module(module_name: &str) -> bool {
     for &log_module in LOG_FOLLOWING_MODULES {
         if module_name.starts_with(log_module) {
