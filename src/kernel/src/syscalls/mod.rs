9762f922ef448fc86c1ea4621d4d01a57c106cb3
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2023-11-05 12:30:00 +0100

    Install syscall handler in Kernel

--- src/kernel/src/syscalls/mod.rs
+++ src/kernel/src/syscalls/mod.rs
@@ -1,20 +1,17 @@
-use common::syscalls::trap_frame::{Register, TrapFrame};
+use common::syscalls::{kernel::Syscalls, trap_frame::TrapFrame};
 
 use crate::println;
 
-const SYSCALL_NR_REG: Register = Register::a7;
-const SYSCALL_ARG1_REG: Register = Register::a0;
+struct SyscallHandler;
 
-pub fn handle_syscall(trap_frame: &mut TrapFrame) {
-    let syscall_nr = trap_frame[SYSCALL_NR_REG];
-    let arg1 = trap_frame[SYSCALL_ARG1_REG];
-
-    match syscall_nr {
-        0 => {
-            println!("Process prints: {}", arg1 as u8 as char);
-        }
-        _ => {
-            panic!("Unknown syscall nr: {}", syscall_nr);
-        }
+impl common::syscalls::kernel::Syscalls for SyscallHandler {
+    #[allow(non_snake_case)]
+    fn WRITE_CHAR(c: u8) -> isize {
+        println!("Process prints: {}", c as char);
+        0
     }
+}
+
+pub fn handle_syscall(trap_frame: &mut TrapFrame) -> isize {
+    SyscallHandler::handle(trap_frame)
 }
