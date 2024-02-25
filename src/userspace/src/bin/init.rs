d77ea4dcbdc7852737926acfecdf2cb4965687b5
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2024-02-25 20:50:20 +0100

    yash: rename shell to yash
    yash stands for "yet another shell"

--- src/userspace/src/bin/init.rs
+++ src/userspace/src/bin/init.rs
@@ -10,7 +10,7 @@ extern crate userspace;
 fn main() {
     println!("init process started");
     println!("starting shell");
-    let shell_name = "shell";
+    let shell_name = "yash";
     let shell_pid = sys_execute(&shell_name.as_bytes()[0], shell_name.len()).unwrap();
     sys_wait(shell_pid as u64).unwrap();
     println!("Initial shell has exited...");
