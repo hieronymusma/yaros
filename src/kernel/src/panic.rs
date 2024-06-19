782d3c48a91f3683e3268ec39d6261cc67e3a9bc
Maurice Hieronymus <maurice_hieronymus@yahoo.de>
2024-06-19 19:49:50 +0200

    panic: Abort if we encounter a triple fault

--- src/kernel/src/panic.rs
+++ src/kernel/src/panic.rs
@@ -1,10 +1,15 @@
 #![cfg_attr(miri, allow(unused_imports))]
 use crate::{println, test::qemu_exit};
 use core::panic::PanicInfo;
+use core::sync::atomic::AtomicU8;
+
+static PANIC_COUNTER: AtomicU8 = AtomicU8::new(0);
 
 #[cfg(not(miri))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    abort_if_triple_fault();
+
     crate::cpu::disable_gloabl_interrupts();
     println!("");
     crate::debug::dump_current_state();
@@ -17,4 +22,13 @@ fn panic(info: &PanicInfo) -> ! {
     }
 
     qemu_exit::exit_failure(1);
+}
+
+fn abort_if_triple_fault() {
+    let current = PANIC_COUNTER.fetch_add(1, core::sync::atomic::Ordering::SeqCst);
+
+    if current >= 3 {
+        println!("TRIPLE FAULT");
+        qemu_exit::exit_failure(1);
+    }
 }
