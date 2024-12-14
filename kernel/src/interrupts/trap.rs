1014cf33da88409bd405b7f5fd7ac91d49cd134a
Maurice Hieronymus <github@ilovebinary.com>
2024-12-14 10:33:23 +0100

    Refactor exception handling function

--- kernel/src/interrupts/trap.rs
+++ kernel/src/interrupts/trap.rs
@@ -6,9 +6,10 @@ use crate::{
     io::{stdin_buf::STDIN_BUFFER, uart},
     memory::linker_information::LinkerInformation,
     processes::scheduler::{self, get_current_process, schedule},
-    syscalls::handle_syscall,
+    syscalls::{self},
     warn,
 };
+use alloc::string::ToString;
 use common::syscalls::trap_frame::{Register, TrapFrame};
 use core::panic;
 
@@ -37,6 +38,65 @@ fn handle_external_interrupt() {
     }
 }
 
+fn handle_syscall(sepc: usize, trap_frame: &mut TrapFrame) {
+    let nr = trap_frame[Register::a0];
+    let arg1 = trap_frame[Register::a1];
+    let arg2 = trap_frame[Register::a2];
+    let arg3 = trap_frame[Register::a3];
+    let ret = syscalls::handle_syscall(nr, arg1, arg2, arg3);
+    if let Some((ret1, ret2)) = ret {
+        trap_frame[Register::a0] = ret1;
+        trap_frame[Register::a1] = ret2;
+        cpu::write_sepc(sepc + 4); // Skip the ecall instruction
+    }
+    // In case our current process was set to waiting state we need to reschedule
+    if let Some(process) = get_current_process()
+        && process.lock().get_state().is_waiting()
+    {
+        schedule();
+    }
+}
+
+fn warn_on_stackoverflow(cause: InterruptCause, stval: usize) {
+    if cause.is_stack_overflow(stval) {
+        let guard_range = LinkerInformation::__start_stack_overflow_guard()
+            ..LinkerInformation::__start_kernel_stack();
+        warn!(
+            "DANGER! STACK OVERFLOW DETECTED! stval={:p} inside guard page {:p}-{:p}",
+            stval as *const (), guard_range.start as *const (), guard_range.end as *const ()
+        );
+    }
+}
+
+fn handle_unhandled_exception(
+    cause: InterruptCause,
+    stval: usize,
+    sepc: usize,
+    trap_frame: &mut TrapFrame,
+) {
+    let current_process = get_current_process();
+    let mut is_userspace_address = "NO ACTIVE PROCESS".to_string();
+    let mut process_name = is_userspace_address.clone();
+    if let Some(current_process) = current_process {
+        let current_process = current_process.lock();
+        is_userspace_address = format!(
+            "{}",
+            current_process.get_page_table().is_userspace_address(sepc)
+        );
+        process_name = current_process.get_name().to_string();
+    }
+    panic!(
+            "Unhandled exception!\nName: {}\nException code: {}\nstval: 0x{:x}\nsepc: 0x{:x}\nFrom Userspace: {}\nProcess name: {}\nTrap Frame: {:?}",
+            cause.get_reason(),
+            cause.get_exception_code(),
+            stval,
+            sepc,
+            is_userspace_address,
+            process_name,
+            trap_frame
+        );
+}
+
 #[no_mangle]
 extern "C" fn handle_exception(
     cause: InterruptCause,
@@ -44,62 +104,10 @@ extern "C" fn handle_exception(
     sepc: usize,
     trap_frame: &mut TrapFrame,
 ) {
+    warn_on_stackoverflow(cause, stval);
     match cause.get_exception_code() {
-        ENVIRONMENT_CALL_FROM_U_MODE => {
-            let nr = trap_frame[Register::a0];
-            let arg1 = trap_frame[Register::a1];
-            let arg2 = trap_frame[Register::a2];
-            let arg3 = trap_frame[Register::a3];
-            let ret = handle_syscall(nr, arg1, arg2, arg3);
-            if let Some((ret1, ret2)) = ret {
-                trap_frame[Register::a0] = ret1;
-                trap_frame[Register::a1] = ret2;
-                cpu::write_sepc(sepc + 4); // Skip the ecall instruction
-            }
-            // In case our current process was set to waiting state we need to reschedule
-            if let Some(process) = get_current_process()
-                && process.lock().get_state().is_waiting()
-            {
-                schedule();
-            }
-        }
-        _ => {
-            if cause.is_stack_overflow(stval) {
-                let guard_range = LinkerInformation::__start_stack_overflow_guard()
-                    ..LinkerInformation::__start_kernel_stack();
-                warn!(
-                    "DANGER! STACK OVERFLOW DETECTED! stval={:p} inside guard page {:p}-{:p}",
-                    stval as *const (),
-                    guard_range.start as *const (),
-                    guard_range.end as *const ()
-                );
-            }
-            let current_process = get_current_process();
-            if let Some(current_process) = current_process {
-                let current_process = current_process.lock();
-                panic!(
-                    "Unhandled exception!\nName: {}\nException code: {}\nstval: 0x{:x}\nsepc: 0x{:x}\nFrom Userspace: {}\nProcess name: {}\nTrap Frame: {:?}",
-                    cause.get_reason(),
-                    cause.get_exception_code(),
-                    stval,
-                    sepc,
-                    current_process.get_page_table().is_userspace_address(sepc),
-                    current_process.get_name(),
-                    trap_frame
-                );
-            } else {
-                panic!(
-                    "Unhandled exception!\nName: {}\nException code: {}\nstval: 0x{:x}\nsepc: 0x{:x}\nFrom Userspace: {}\nProcess name: {}\nTrap Frame: {:?}",
-                    cause.get_reason(),
-                    cause.get_exception_code(),
-                    stval,
-                    sepc,
-                    false,
-                    "No scheduled process",
-                    trap_frame
-                );
-            }
-        }
+        ENVIRONMENT_CALL_FROM_U_MODE => handle_syscall(sepc, trap_frame),
+        _ => handle_unhandled_exception(cause, stval, sepc, trap_frame),
     }
 }
 
